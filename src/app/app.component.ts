5b6e08549fef9dd4e4013ed6f9f05506cdc804bb
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-04-07 01:53:12 +0600

    app module and wikipedia service is configured for app call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { WikipediaService } from "./wikipedia.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'wiki-search';
+  constructor(private wikipedia: WikipediaService) {}
+
+  onSubmit(searchText: string) {
+    this.wikipedia.search(searchText).subscribe((response) => {
+      console.log(response);
+    });
+  }
 }
